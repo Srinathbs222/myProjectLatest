10cc1df04e8ee17347a93dd33d23445e369b3935
Srinathbs222 <srinathbs222@gmail.com>
2024-04-07 12:46:15 +0530

    add title

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
 return (
 <main
 className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
+    <h1 className="text-4xl font-bold">Add Numbers</h1>
 <AddNumbers />
 </main>
 )
